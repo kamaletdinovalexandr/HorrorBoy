2dda1fdae77543d0db26291639030e27b393c939
a.kamaletdinov <a.kamaletdinov@corpwebgames.com>
2019-06-28 21:54:40 +0300

    init

---
+++ Assets/Scripts/CameraFolower.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFolower : MonoBehaviour {
+    [SerializeField] Transform _transform;
+
+    private void FixedUpdate() {
+        var newX = _transform.position.x;
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    [SerializeField] Rigidbody _rb;
+    [SerializeField] float _speed = 5f;
+    [SerializeField] GameObject _lightning;
+
+    private void Update() {
+        var newPosition = new Vector3(_speed * Time.deltaTime, 0, 0);
+
+        if (Input.GetMouseButtonDown(1)) {
+            newPosition = new Vector3(newPosition.x, 200, newPosition.z);
+        }
+
+       
+        if (Input.GetMouseButtonDown(0)) {
+            //Shoot();
+        }
+        _rb.AddForce(newPosition);
+    }
+
+    private void Shoot() {
+        Instantiate(_lightning);
+    }
+}
